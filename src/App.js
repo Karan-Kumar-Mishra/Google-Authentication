c8a0976c387cf8c4d7952578902bfdc2f79a8b19
KaranMishra <93134411+Karan-Kumar-Mishra@users.noreply.github.com>
2024-09-08 13:39:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
+
+function App() {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  // log out function to log the user out of google and set the profile array to null
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
+
+  return (
+    <div className="flex bg-slate-900  justify-center items-center h-screen w-screen text-white ">
+
+      <br />
+      <br />
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <br />
+          <br />
+          <button
+         className="bg-slate-300 p-[5px] text-neutral-950 font-semibold w-32 rounded-3xl"
+           onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        
+         <button
+         className="bg-slate-300  p-[2px] text-neutral-950 font-semibold w-32 rounded-3xl"
+         onClick={()=>{
+            login()
+         }} > Login with goole</button>
+      )}
+    </div>
+  );
+}
+export default App;
